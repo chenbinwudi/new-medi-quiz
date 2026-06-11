d880726d945b30725ed72531c7fee2c701e6170e
chenbinwudi <chenbinwudi@aliyun.com>
2026-06-11 12:39:14 +0800

    feat: add material detail page

--- miniprogram/pages/material-detail/material-detail.js
+++ miniprogram/pages/material-detail/material-detail.js
@@ -1,3 +1,37 @@
+const { materials } = require('../../data/materials');
+const { toggleFavorite } = require('../../services/study');
+
 Page({
-  data: {}
+  data: {
+    material: null,
+    activeTab: 'intro',
+    favorited: false
+  },
+
+  onLoad(options) {
+    const material = materials.find((item) => item.id === options.id) || materials[0];
+    this.setData({ material });
+  },
+
+  switchTab(event) {
+    this.setData({ activeTab: event.currentTarget.dataset.tab });
+  },
+
+  toggleFavorite() {
+    toggleFavorite({ targetType: 'material', targetId: this.data.material.id })
+      .then((res) => this.setData({ favorited: res.favorited }))
+      .catch(() => wx.showToast({ title: '收藏同步失败', icon: 'none' }));
+  },
+
+  startLearning() {
+    wx.showToast({ title: '资料阅读功能建设中', icon: 'none' });
+  },
+
+  onShareAppMessage() {
+    const material = this.data.material;
+    return {
+      title: material ? material.title : '医考通资料',
+      path: `/pages/material-detail/material-detail?id=${material ? material.id : ''}`
+    };
+  }
 });
